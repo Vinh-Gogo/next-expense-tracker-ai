2e86ee2e3756c5e7b972f78ff6d4aa71d7ac04a8
vinh-gogo <lea26462@gmail.com>
2025-08-21 10:44:08 +0700

    Complete Navbar component and guest, about and pages

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,16 @@
+import { currentUser } from '@clerk/nextjs/server'
 import React from 'react'
+import Guest from '@/components/Guest'
 
-export default function HomePage() {
+export default async function HomePage() {
+    const user = await currentUser();
+
+    if (!user) {
+        return <Guest />
+
     return (
-        <div className='text-red-600'>
+        <div className='text-red-600 dark:text-red-300'>
             HomePage
         </div>
     )
-}
+}}
